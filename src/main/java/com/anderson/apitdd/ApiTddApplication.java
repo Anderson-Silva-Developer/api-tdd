76a2e1fd050b2a3ee2f7677af915048e7918d34b
asc <silvaandersonpi@gmail.com>
2022-08-06 11:28:06 -0300

    projeto api-tdd

---
+++ src/main/java/com/anderson/apitdd/ApiTddApplication.java
@@ -0,0 +1,14 @@
+package com.anderson.apitdd;
+
+import com.anderson.apitdd.entities.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTddApplication.class, args);
+    }
+
+}

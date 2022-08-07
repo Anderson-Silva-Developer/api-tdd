2778448744cf31adeac88a440afee347d1505e48
asc <silvaandersonpi@gmail.com>
2022-08-06 23:09:55 -0300

    feat:adicionado a classe de UserRepository

---
+++ src/main/java/com/anderson/apitdd/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.anderson.apitdd.repository;
+
+import com.anderson.apitdd.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+}
